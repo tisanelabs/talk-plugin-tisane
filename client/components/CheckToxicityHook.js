5fcd13e1ac368f3ec449aa9f1e7a7c039fb6064a
vadim-berman <vadim.berman@tisane.ai>
2019-06-07 20:18:45 +0800

    Debugging

--- client/components/CheckToxicityHook.js
+++ client/components/CheckToxicityHook.js
@@ -33,7 +33,10 @@ export default class CheckToxicityHook extends React.Component {
         )
       ) {
         const comment = result.createComment.comment;
-        console.log(Object.keys(comment));
+        if (!comment)
+          console.log("No comment!!!");
+        else
+          console.log(Object.keys(comment));
         if (comment.metadata && comment.metadata.report) {
           this.props.notify('error', t('talk-plugin-toxic-tisane.straight_to_moderation'));
         } else {
